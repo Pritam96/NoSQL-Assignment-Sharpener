a743ebaacdf97a227a66c40f97d684f71f8a9998
Pritam96 <pritammondal96@gmail.com>
2024-01-09 14:02:32 +0530

    Creating MongoDB connection & Creating Products

--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+
+  MongoClient.connect('mongodb+srv://pritam123:pritam123@cluster0.n6kdjls.mongodb.net/mongoExample?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    }).catch(err => {
+      console.log(err);
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found';
+};
+
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
