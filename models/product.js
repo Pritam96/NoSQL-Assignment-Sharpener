9b55a9cc1d1594eae92823deecd57479f29dc6cb
Pritam96 <pritammondal96@gmail.com>
2024-01-09 20:59:40 +0530

    MongDB CRUD Operations - Fetching all products / Find a product by Id / Edit a product / Delete a product

--- models/product.js
+++ models/product.js
@@ -1,19 +1,73 @@
-const { getDb } = require('../util/database.js');
+const { getDb } = require("../util/database.js");
+const mongodb = require("mongodb");
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, _id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = _id ? new mongodb.ObjectId(_id) : null;
   }
 
   save() {
     const db = getDb();
-    return db.collection('products').insertOne(this)
-      .then(result => {
+    let dbOperation;
+    if (this._id) {
+      dbOperation = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOperation = db.collection("products").insertOne(this);
+    }
+    return dbOperation
+      .then((result) => {
         console.log(result);
-      }).catch(err => {
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(productId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(productId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(productId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(productId) })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
         console.log(err);
       });
   }
--- util/database.js
+++ util/database.js
@@ -1,16 +1,18 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
 const mongoConnect = (callback) => {
-
-  MongoClient.connect('mongodb+srv://pritam123:pritam123@cluster0.n6kdjls.mongodb.net/mongoExample?retryWrites=true&w=majority')
-    .then(client => {
-      console.log('Connected!');
+  MongoClient.connect(
+    "mongodb+srv://pritam123:pritam123@cluster0.n6kdjls.mongodb.net/mongoExample?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
       _db = client.db();
       callback();
-    }).catch(err => {
+    })
+    .catch((err) => {
       console.log(err);
     });
 };
@@ -19,9 +21,8 @@ const getDb = () => {
   if (_db) {
     return _db;
   }
-  throw 'No database found';
+  throw "No database found";
 };
-
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
